e10d1814b80499a78e30cea7e1d82f7e1be56c30
J2436 <wackejacky@gmail.com>
2020-05-20 21:32:13 -0700

    updated help

--- cmds/help.js
+++ cmds/help.js
@@ -1,21 +1,22 @@
 const menus = {
-    main: `
+  main: `
       outside [command] <options>
   
       today .............. show weather for today
+      forecast ........... show 7 day forecast
       version ............ show package version
       help ............... show help menu for a command`,
-  
-    today: `
+
+  today: `
       outside today <options>
-  
       --location, -l ..... the location to use`,
-  }
-  
-  module.exports = (args) => {
-    const subCmd = args._[0] === 'help'
-      ? args._[1]
-      : args._[0]
-  
-    console.log(menus[subCmd] || menus.main)
-  }
+
+  forecast: `
+    outside forecast <options> 
+    -- location, -l ..... the location to use`,
+};
+
+module.exports = (args) => {
+  const subCmd = args._[0] === "help" ? args._[1] : args._[0];
+  console.log(menus[subCmd] || menus.main);
+};
