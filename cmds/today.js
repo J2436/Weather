f68591e94604ad22a8e2bf28b4de0d502c35bdae
J2436 <wackejacky@gmail.com>
2020-05-20 21:32:47 -0700

    cleaned up

--- cmds/today.js
+++ cmds/today.js
@@ -1,20 +1,18 @@
-const ora = require('ora')
-const getWeather = require('../utils/weather')
+const ora = require("ora");
+const getWeather = require("../utils/getToday");
 
 module.exports = async (args) => {
-  const spinner = ora().start()
-
+  const spinner = ora().start();
   try {
-    const location = args.location || args.l
-    const weather = await getWeather(location)
+    const location = args.location || args.l;
+    const weather = await getWeather(location);
 
-    spinner.stop()
+    spinner.stop();
 
-    console.log(`Current conditions in ${location}:`)
-    console.log(`\t${weather.main.temp}° ${weather.weather[0].description}`)
+    console.log(`Current conditions in ${location}:`);
+    console.log(`\t${weather.main.temp}°F ${weather.weather[0].description}`);
   } catch (err) {
-    spinner.stop()
-
-    console.error(err)
+    spinner.stop();
+    console.error(err);
   }
-}
+};
