7e9cc714667fb40ef38ac6a85f3a912da8212e75
J2436 <wackejacky@gmail.com>
2020-05-20 21:49:39 -0700

    added weather description

--- cmds/forecast.js
+++ cmds/forecast.js
@@ -9,6 +9,7 @@ module.exports = async (args) => {
   const forecast = await require("../utils/getForecast")({ lon, lat });
   for (let day of forecast.daily) {
     console.log(new Date(day.dt * 1000).toDateString());
+    console.log(" Weather:", day.weather[0].description);
     console.log(` Morning:  ${day.temp.morn}°F`);
     console.log(` Day    :  ${day.temp.day}°F`);
     console.log(` Evening:  ${day.temp.eve}°F`);
